dd6d439e5b6dec19fde060ce617737eec4d6db31
odeladetunji <odeladetunji@gmail.com>
2021-07-26 22:00:56 +0100

    committing

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const app = express();
 const server = require('http').Server(app);
 const cors = require('cors');
 const path = require('path');
+const bodyParser = require('body-parser')
+
+const movieData = require('./data');
+
 app.use('*', cors());
 
-// const retriveBalance = require('./routes/retrive_balance');
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/movies', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost:27017/moviesdbtest1', {useNewUrlParser: true, useUnifiedTopology: true});
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
@@ -16,30 +19,60 @@ db.once('open', function() {
 
 //schema
 const moviesSchema = new mongoose.Schema({
-    data: String
+    data: {
+        show: {
+            "id": Number,
+            "url": String,
+            "name": String,
+            "language": String,
+            "genres": [
+            
+            ],
+            "status": String,
+            "runtime": Number,
+            "premiered": String,
+            "rating": {
+                "average": Number
+            },
+            "network": {
+                "name": String
+            },
+            "image": {
+                "original": String
+            },
+            "summary": String
+        }
+    }
 });
 
 const moviesModel = mongoose.model('movies', moviesSchema);
 
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false })); // urlencoded form parser
+app.use(bodyParser.json())  // json parser
+app.use(cors());
+
 //routes 
-app.use('/', (request, response) => {
+app.get('', (request, response) => {
     response.send("Movies Server is Running")
 });
 
-app.post('/storemovies', function(req, res){
-    const movies = new moviesModel({data: JSON.stringify(req.body)});
+app.post('/savemovies', function(req, res){
+    console.log(req.body)
+    const movies = new moviesModel({data: req.body});
     movies.save(function(err, movies){
         if (err) throw err;
-        return res.send(res);
+        res.send(movies);
     });
 });
 
 app.get('/getmovies', function(req, res){
-    let movie = moviesModel.find(function(err, movies){
+    let movies = moviesModel.find(function(err, movies){
         if(err) throw err;
-        return res.send(JSON.parse(movies))
+        res.send(movies)
     });
-})
+});
 
 server.listen(8025, function(){
     console.log("Movies Server is Running");
---
+++ data.js
@@ -0,0 +1,356 @@
+
+const moviesData = () => {
+    const data = [
+        {
+            "score": 0.6487603,
+            "show": {
+                "id": 19678,
+                "url": "https://www.tvmaze.com/shows/19678/p-valley",
+                "name": "P-Valley",
+                "type": "Scripted",
+                "language": "English",
+                "genres": [
+                    "Drama",
+                    "Crime"
+                ],
+                "status": "Running",
+                "runtime": 60,
+                "averageRuntime": 58,
+                "premiered": "2020-07-12",
+                "officialSite": "https://www.starz.com/us/en/series/46156",
+                "schedule": {
+                    "time": "20:00",
+                    "days": [
+                        "Sunday"
+                    ]
+                },
+                "rating": {
+                    "average": 4.8
+                },
+                "weight": 86,
+                "network": {
+                    "id": 17,
+                    "name": "Starz",
+                    "country": {
+                        "name": "United States",
+                        "code": "US",
+                        "timezone": "America/New_York"
+                    }
+                },
+                "webChannel": null,
+                "dvdCountry": null,
+                "externals": {
+                    "tvrage": null,
+                    "thetvdb": 356405,
+                    "imdb": "tt9340526"
+                },
+                "image": {
+                    "medium": "https://static.tvmaze.com/uploads/images/medium_portrait/262/656535.jpg",
+                    "original": "https://static.tvmaze.com/uploads/images/original_untouched/262/656535.jpg"
+                },
+                "summary": "<p><b>P-Valley</b> is a scripted one-hour drama that takes an unflinching and unapologetic look at the lives of pole dancers working down in the Dirty Delta. Set in an infamous Mississippi \"shake junt\" called The Pynk, <i>P-Valley</i> is an intimate window to the unexplored southern strip club world, where the desires of pro-ballers and politicians collide with the dreams of five brave women. For these women, the line between performance and reality becomes blurred, as the drama of their outside lives threaten their onstage personas. On the quest for money, power, and respect, each woman must make a choice. For some it's a step down, for others a step up, as The Pynk stage constantly shape-shifts between a platform for freedom and a gilded cage.</p>",
+                "updated": 1605392131,
+                "_links": {
+                    "self": {
+                        "href": "https://api.tvmaze.com/shows/19678"
+                    },
+                    "previousepisode": {
+                        "href": "https://api.tvmaze.com/episodes/1906539"
+                    }
+                }
+            }
+        },
+        {
+            "score": 0.48815227,
+            "show": {
+                "id": 22667,
+                "url": "https://www.tvmaze.com/shows/22667/p-king-duckling",
+                "name": "P. King Duckling",
+                "type": "Animation",
+                "language": "English",
+                "genres": [
+                    "Children"
+                ],
+                "status": "Running",
+                "runtime": 15,
+                "averageRuntime": 15,
+                "premiered": "2016-11-07",
+                "officialSite": null,
+                "schedule": {
+                    "time": "17:00",
+                    "days": [
+                        "Monday",
+                        "Tuesday",
+                        "Wednesday",
+                        "Thursday",
+                        "Friday"
+                    ]
+                },
+                "rating": {
+                    "average": null
+                },
+                "weight": 0,
+                "network": {
+                    "id": 1039,
+                    "name": "Disney Junior",
+                    "country": {
+                        "name": "United States",
+                        "code": "US",
+                        "timezone": "America/New_York"
+                    }
+                },
+                "webChannel": null,
+                "dvdCountry": null,
+                "externals": {
+                    "tvrage": null,
+                    "thetvdb": 321831,
+                    "imdb": null
+                },
+                "image": {
+                    "medium": "https://static.tvmaze.com/uploads/images/medium_portrait/83/209696.jpg",
+                    "original": "https://static.tvmaze.com/uploads/images/original_untouched/83/209696.jpg"
+                },
+                "summary": "<p><b>P. King Duckling</b> is a Chinese/American animated television series created by Josh Selig. The series debuted on Disney Junior in the United States on November 7, 2016. P. King Duckling is about the title character, a young duck, who has adventures in the town of Hilly Hole with his two best friends, Chumpkins and Wombat.</p>",
+                "updated": 1503473563,
+                "_links": {
+                    "self": {
+                        "href": "https://api.tvmaze.com/shows/22667"
+                    },
+                    "previousepisode": {
+                        "href": "https://api.tvmaze.com/episodes/1279614"
+                    }
+                }
+            }
+        },
+        {
+            "score": 0.47261956,
+            "show": {
+                "id": 5122,
+                "url": "https://www.tvmaze.com/shows/5122/przypadki-cezarego-p",
+                "name": "Przypadki Cezarego P.",
+                "type": "Scripted",
+                "language": "Polish",
+                "genres": [],
+                "status": "Ended",
+                "runtime": 30,
+                "averageRuntime": 30,
+                "premiered": "2015-03-08",
+                "officialSite": null,
+                "schedule": {
+                    "time": "15:50",
+                    "days": [
+                        "Saturday"
+                    ]
+                },
+                "rating": {
+                    "average": null
+                },
+                "weight": 0,
+                "network": {
+                    "id": 392,
+                    "name": "TV Puls",
+                    "country": {
+                        "name": "Poland",
+                        "code": "PL",
+                        "timezone": "Europe/Warsaw"
+                    }
+                },
+                "webChannel": null,
+                "dvdCountry": null,
+                "externals": {
+                    "tvrage": null,
+                    "thetvdb": 296012,
+                    "imdb": null
+                },
+                "image": {
+                    "medium": "https://static.tvmaze.com/uploads/images/medium_portrait/45/113502.jpg",
+                    "original": "https://static.tvmaze.com/uploads/images/original_untouched/45/113502.jpg"
+                },
+                "summary": "<p><b>Przypadki Cezarego P. </b>tells the story of ancient icon of Polish cinema - Cezarego P., who wants to return to the top, but gets into big trouble, as well as losing the entire property. The man decides to move to apartment building in Prague to start a whole new life. On site he meets the beautiful neighbor, Agata, and her malicious son. Head of the acting agency Irena and agent Łukasz will help him in his ups and downs.</p>",
+                "updated": 1544819962,
+                "_links": {
+                    "self": {
+                        "href": "https://api.tvmaze.com/shows/5122"
+                    },
+                    "previousepisode": {
+                        "href": "https://api.tvmaze.com/episodes/319463"
+                    }
+                }
+            }
+        },
+        {
+            "score": 0.44325048,
+            "show": {
+                "id": 38435,
+                "url": "https://www.tvmaze.com/shows/38435/chris-p-duck",
+                "name": "Chris P. Duck",
+                "type": "Animation",
+                "language": "English",
+                "genres": [],
+                "status": "Ended",
+                "runtime": 4,
+                "averageRuntime": 4,
+                "premiered": "2018-03-04",
+                "officialSite": "http://cartoonhangover.com/shows/chris-p-duck/",
+                "schedule": {
+                    "time": "",
+                    "days": []
+                },
+                "rating": {
+                    "average": null
+                },
+                "weight": 0,
+                "network": null,
+                "webChannel": {
+                    "id": 284,
+                    "name": "Cartoon Hangover",
+                    "country": {
+                        "name": "United States",
+                        "code": "US",
+                        "timezone": "America/New_York"
+                    }
+                },
+                "dvdCountry": null,
+                "externals": {
+                    "tvrage": null,
+                    "thetvdb": 346077,
+                    "imdb": null
+                },
+                "image": {
+                    "medium": "https://static.tvmaze.com/uploads/images/medium_portrait/165/414384.jpg",
+                    "original": "https://static.tvmaze.com/uploads/images/original_untouched/165/414384.jpg"
+                },
+                "summary": "<p>Lovable moron, Chris, is a well-meaning duck who just wants to make friends and bring joy to his seedy run-down London park. Unfortunately, although he has the best intentions, his cluelessness and desperation to please are frequently exploited by his self-serving friends -- paranoid squirrel, Tracey and scumbag mallard, Roger -- inevitably leading to chaos, destruction and random acts of arson.</p>",
+                "updated": 1571161140,
+                "_links": {
+                    "self": {
+                        "href": "https://api.tvmaze.com/shows/38435"
+                    },
+                    "previousepisode": {
+                        "href": "https://api.tvmaze.com/episodes/1520157"
+                    }
+                }
+            }
+        },
+        {
+            "score": 0.41278416,
+            "show": {
+                "id": 29699,
+                "url": "https://www.tvmaze.com/shows/29699/la-familia-p-luche",
+                "name": "La Familia P. Luche",
+                "type": "Scripted",
+                "language": "Spanish",
+                "genres": [
+                    "Comedy"
+                ],
+                "status": "Ended",
+                "runtime": 30,
+                "averageRuntime": 30,
+                "premiered": "2002-08-07",
+                "officialSite": null,
+                "schedule": {
+                    "time": "21:00",
+                    "days": [
+                        "Sunday"
+                    ]
+                },
+                "rating": {
+                    "average": null
+                },
+                "weight": 0,
+                "network": {
+                    "id": 529,
+                    "name": "Las Estrellas",
+                    "country": {
+                        "name": "Mexico",
+                        "code": "MX",
+                        "timezone": "America/Monterrey"
+                    }
+                },
+                "webChannel": null,
+                "dvdCountry": null,
+                "externals": {
+                    "tvrage": null,
+                    "thetvdb": 237291,
+                    "imdb": "tt1244672"
+                },
+                "image": {
+                    "medium": "https://static.tvmaze.com/uploads/images/medium_portrait/117/294975.jpg",
+                    "original": "https://static.tvmaze.com/uploads/images/original_untouched/117/294975.jpg"
+                },
+                "summary": "<p><b>La Familia P. Luche </b>(The Plush Family) is a Mexican television sitcom created by Eugenio Derbez. The series first aired in 2002 on the channel Canal de las Estrellas, and was later syndicated on the American channel Univision the next year. It is a spin-off of a regular sketch on the XHDRBZ comedy-sketch program. In the series, the characters live in a fictional city called \"Ciudad P. Luche\" where clothes and other objects are wrapped in plush. The word P. Luche, is a pun to the word plush in Spanish: peluche. The show is similar in style to the American sitcom Married... With Children, in that it depicts a comically dysfunctional family. La familia P. Luche, however, plays more like a live-action cartoon, complete with colorful sets and comic sound effects. The sitcom won a TVyNovelas Award for \"Best Comedy Program\" in 2008. Its working-class rival Una familia de diez, was also nominated.</p>",
+                "updated": 1505363813,
+                "_links": {
+                    "self": {
+                        "href": "https://api.tvmaze.com/shows/29699"
+                    },
+                    "previousepisode": {
+                        "href": "https://api.tvmaze.com/episodes/1225710"
+                    }
+                }
+            }
+        },
+        {
+            "score": 0.40402478,
+            "show": {
+                "id": 26781,
+                "url": "https://www.tvmaze.com/shows/26781/p-diddy-presents-the-bad-boys-of-comedy",
+                "name": "P. Diddy Presents the Bad Boys of Comedy",
+                "type": "Variety",
+                "language": "English",
+                "genres": [
+                    "Comedy"
+                ],
+                "status": "Ended",
+                "runtime": 60,
+                "averageRuntime": 60,
+                "premiered": "2005-06-10",
+                "officialSite": null,
+                "schedule": {
+                    "time": "21:00",
+                    "days": [
+                        "Friday"
+                    ]
+                },
+                "rating": {
+                    "average": null
+                },
+                "weight": 0,
+                "network": {
+                    "id": 8,
+                    "name": "HBO",
+                    "country": {
+                        "name": "United States",
+                        "code": "US",
+                        "timezone": "America/New_York"
+                    }
+                },
+                "webChannel": null,
+                "dvdCountry": null,
+                "externals": {
+                    "tvrage": null,
+                    "thetvdb": 125521,
+                    "imdb": "tt0460667"
+                },
+                "image": {
+                    "medium": "https://static.tvmaze.com/uploads/images/medium_portrait/105/263532.jpg",
+                    "original": "https://static.tvmaze.com/uploads/images/original_untouched/105/263532.jpg"
+                },
+                "summary": "<p>Underground comedians perform before a live audience at the Brooklyn Academy of Music in New York.</p>",
+                "updated": 1573613029,
+                "_links": {
+                    "self": {
+                        "href": "https://api.tvmaze.com/shows/26781"
+                    },
+                    "previousepisode": {
+                        "href": "https://api.tvmaze.com/episodes/1122249"
+                    }
+                }
+            }
+        }
+    ]
+
+    return JSON.stringify(data);
+}
+
+module.exports = moviesData;
